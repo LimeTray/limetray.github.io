4e8a8bba49195707eea1d43a1f929f73078ec46f
lt-login <lt.login@limetray.com>
2026-02-24 13:30:16 +0530

    Deployed master Storybook from Jenkins: https://deployment.limetray.com/ | Build Number: 16

---
+++ react-storybook/assets/LtCardV2.stories-dbf040a0.js
@@ -0,0 +1,21 @@
+import{j as r}from"./jsx-runtime-8406ef1e.js";import{I as n}from"./Icon-33b7fdc8.js";import{L}from"./ltCardV2-c06f273d.js";import{T as A}from"./tooltip-a033bc48.js";import"./index-ad58220e.js";import"./_commonjsHelpers-de833af9.js";import"./extends-4c19d496.js";import"./inheritsLoose-5494d9cc.js";import"./factories-f5adea19.js";import"./getElementType-ff0d42f5.js";import"./childrenUtils-dd3b0ba5.js";import"./Header-feb25eb8.js";import"./Label-541ced09.js";import"./ModernAutoControlledComponent-fe68b7fb.js";import"./assertThisInitialized-081f9914.js";import"./htmlPropsUtils-923b414b.js";import"./includes-8058702b.js";import"./objectWithoutPropertiesLoose-f3b3ace0.js";import"./index-4fee7e45.js";import"./Portal-50130825.js";import"./Card-919855e3.js";import"./map-20538cdf.js";import"./Popup-706ce128.js";import"./pick-4047f9a6.js";import"./_baseSet-bea29546.js";import"./index-f77928b3.js";import"./_arrayReduce-865b0d3f.js";import"./toPropertyKey-4015efc6.js";const rr={component:L,title:"LTCardV2",tags:["autodocs"],argTypes:{header:{control:"text"},subheader:{control:"text"},onClick:{action:"clicked"}}},i=P=>r.jsx("div",{style:{width:"300px"},children:r.jsx(L,{...P})}),e=i.bind({});e.args={header:"KPT",children:r.jsx("div",{children:"Content for KPT Card"})};const o=i.bind({});o.args={children:r.jsx("div",{children:"without header Card"})};const t=i.bind({});t.args={header:"KPT",subheader:"Kitchen Preparation Time",children:r.jsx("div",{children:"Content for KPT Card"}),headerAction:r.jsx(n,{name:"filter",size:"small",color:"green"})};const a=i.bind({});a.args={header:"KPT",subheader:"Kitchen Preparation Time",children:r.jsx("div",{children:"Content for KPT Card"}),headerIcon:r.jsx(n,{name:"filter",size:"small",color:"green"}),headerAction:r.jsx(n,{name:"filter",size:"small",color:"green"})};const s=i.bind({});s.args={children:r.jsxs("div",{style:{display:"flex",flexDirection:"column"},children:[r.jsxs("span",{style:{fontSize:"1.4rem",fontWeight:"400",color:"#374151"},children:["Revenue",r.jsx(A,{content:"Tooltip content",position:"top center",trigger:r.jsx(n,{size:"small",name:"info circle",style:{marginLeft:"5px",color:"grey"}})})]}),r.jsx("span",{style:{marginTop:"18px",fontSize:"1.5rem",fontWeight:"700",color:"#374151"},children:"₹ 436,798.79"}),r.jsxs("span",{style:{marginTop:"10px",display:"flex",gap:"10px",fontSize:"0.875rem",fontWeight:"400",color:"#374151"},children:[r.jsx("p",{style:{fontSize:"12px",fontWeight:"500",color:"red"},children:"↓ ₹94332 (-18%)"}),r.jsx("p",{children:"from last week"})]})]})};var d,c,p;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`(args: LTCardProps) => <div style={{
+  width: "300px"
+}}>
+    <LTCard {...args} />
+  </div>`,...(p=(c=e.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var m,l,h;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`(args: LTCardProps) => <div style={{
+  width: "300px"
+}}>
+    <LTCard {...args} />
+  </div>`,...(h=(l=o.parameters)==null?void 0:l.docs)==null?void 0:h.source}}};var x,g,u;t.parameters={...t.parameters,docs:{...(x=t.parameters)==null?void 0:x.docs,source:{originalSource:`(args: LTCardProps) => <div style={{
+  width: "300px"
+}}>
+    <LTCard {...args} />
+  </div>`,...(u=(g=t.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var f,T,C;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`(args: LTCardProps) => <div style={{
+  width: "300px"
+}}>
+    <LTCard {...args} />
+  </div>`,...(C=(T=a.parameters)==null?void 0:T.docs)==null?void 0:C.source}}};var j,y,v;s.parameters={...s.parameters,docs:{...(j=s.parameters)==null?void 0:j.docs,source:{originalSource:`(args: LTCardProps) => <div style={{
+  width: "300px"
+}}>
+    <LTCard {...args} />
+  </div>`,...(v=(y=s.parameters)==null?void 0:y.docs)==null?void 0:v.source}}};const er=["Default","WithoutHeader","HeaderSubHeaderAndAction","HeaderWithIconAndAction","AnalyticCard"];export{s as AnalyticCard,e as Default,t as HeaderSubHeaderAndAction,a as HeaderWithIconAndAction,o as WithoutHeader,er as __namedExportsOrder,rr as default};
---
+++ react-storybook/assets/SelectorGroup.stories-0de725a5.js
@@ -0,0 +1,127 @@
+import{S as d}from"./index-2f16dd7d.js";/* empty css              */import"./jsx-runtime-8406ef1e.js";import"./index-ad58220e.js";import"./_commonjsHelpers-de833af9.js";import"./index-d50b82e2.js";import"./ltRadio-260fa1bb.js";import"./Radio-e2dd228f.js";import"./extends-4c19d496.js";import"./getElementType-ff0d42f5.js";import"./Checkbox-53f402d0.js";import"./inheritsLoose-5494d9cc.js";import"./_baseSet-bea29546.js";import"./factories-f5adea19.js";import"./ModernAutoControlledComponent-fe68b7fb.js";import"./assertThisInitialized-081f9914.js";import"./htmlPropsUtils-923b414b.js";import"./includes-8058702b.js";import"./objectWithoutPropertiesLoose-f3b3ace0.js";import"./index-4fee7e45.js";import"./ThemeContext-71b3bf8d.js";import"./requestIdGenerator-f6efc017.js";const q={component:d,title:"SelectorGroup",tags:["autodocs"]},e={args:{options:[{type:"radio",options:[{key:"veg",value:"Veg"},{key:"nonveg",value:"Non-veg"}]},{type:"radio",options:[{key:"veg",value:"Veg"},{key:"nonveg",value:"Non-veg"}]}]}},n={args:{options:[{label:"Type",type:"checkbox",options:[{key:"veg",value:"Veg"},{key:"nonveg",value:"Non-veg"}]},{label:"Type",type:"checkbox",options:[{key:"veg",value:"Veg"},{key:"nonveg",value:"Non-veg"}]}]}},o={args:{options:[{label:"Services",type:"checkbox",options:[{key:"delivery",value:"Delivery",childrens:[{key:"zomato",value:"Zomato"},{key:"swiggy",value:"Swiggy"}],selectedValues:[]},{key:"takeaway",value:"Take away",selectedValues:[]}]},{label:"Services",type:"checkbox",options:[{key:"delivery",value:"Delivery",selectedValues:[]},{key:"takeaway",value:"Take away",selectedValues:[]}]}]}},a={args:{options:[{label:"Type",type:"radio",options:[{key:"veg",value:"Veg"},{key:"nonveg",value:"Non-veg"}]},{label:"Services",type:"checkbox",options:[{key:"delivery",value:"Delivery",childrens:[{key:"zomato",value:"Zomato"},{key:"swiggy",value:"Swiggy"}],selectedValues:[]},{key:"takeaway",value:"Take away",selectedValues:[]}]},{label:"Services",type:"checkbox",options:[{key:"delivery",value:"Delivery",selectedValues:[]},{key:"takeaway",value:"Take away",selectedValues:[]}]}]}};var t,s,l;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    options: [{
+      type: "radio",
+      options: [{
+        key: "veg",
+        value: "Veg"
+      }, {
+        key: "nonveg",
+        value: "Non-veg"
+      }]
+    }, {
+      type: "radio",
+      options: [{
+        key: "veg",
+        value: "Veg"
+      }, {
+        key: "nonveg",
+        value: "Non-veg"
+      }]
+    }]
+  }
+}`,...(l=(s=e.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};var r,y,i;n.parameters={...n.parameters,docs:{...(r=n.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    options: [{
+      label: "Type",
+      type: "checkbox",
+      options: [{
+        key: "veg",
+        value: "Veg"
+      }, {
+        key: "nonveg",
+        value: "Non-veg"
+      }]
+    }, {
+      label: "Type",
+      type: "checkbox",
+      options: [{
+        key: "veg",
+        value: "Veg"
+      }, {
+        key: "nonveg",
+        value: "Non-veg"
+      }]
+    }]
+  }
+}`,...(i=(y=n.parameters)==null?void 0:y.docs)==null?void 0:i.source}}};var p,v,c;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    options: [{
+      label: "Services",
+      type: "checkbox",
+      options: [{
+        key: "delivery",
+        value: "Delivery",
+        childrens: [{
+          key: "zomato",
+          value: "Zomato"
+        }, {
+          key: "swiggy",
+          value: "Swiggy"
+        }],
+        selectedValues: []
+      }, {
+        key: "takeaway",
+        value: "Take away",
+        selectedValues: []
+      }]
+    }, {
+      label: "Services",
+      type: "checkbox",
+      options: [{
+        key: "delivery",
+        value: "Delivery",
+        selectedValues: []
+      }, {
+        key: "takeaway",
+        value: "Take away",
+        selectedValues: []
+      }]
+    }]
+  }
+}`,...(c=(v=o.parameters)==null?void 0:v.docs)==null?void 0:c.source}}};var u,k,g;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    options: [{
+      label: "Type",
+      type: "radio",
+      options: [{
+        key: "veg",
+        value: "Veg"
+      }, {
+        key: "nonveg",
+        value: "Non-veg"
+      }]
+    }, {
+      label: "Services",
+      type: "checkbox",
+      options: [{
+        key: "delivery",
+        value: "Delivery",
+        childrens: [{
+          key: "zomato",
+          value: "Zomato"
+        }, {
+          key: "swiggy",
+          value: "Swiggy"
+        }],
+        selectedValues: []
+      }, {
+        key: "takeaway",
+        value: "Take away",
+        selectedValues: []
+      }]
+    }, {
+      label: "Services",
+      type: "checkbox",
+      options: [{
+        key: "delivery",
+        value: "Delivery",
+        selectedValues: []
+      }, {
+        key: "takeaway",
+        value: "Take away",
+        selectedValues: []
+      }]
+    }]
+  }
+}`,...(g=(k=a.parameters)==null?void 0:k.docs)==null?void 0:g.source}}};const F=["RadioButtonsGroup","CheckboxGroup","CheckboxNestedGroup","CheckboxAndRadioButtonNestedGroup"];export{a as CheckboxAndRadioButtonNestedGroup,n as CheckboxGroup,o as CheckboxNestedGroup,e as RadioButtonsGroup,F as __namedExportsOrder,q as default};
