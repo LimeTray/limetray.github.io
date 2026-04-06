a295551a6df5c6fbe0a9ea75845668037e282dfe
lt-login <lt.login@limetray.com>
2026-04-07 00:06:11 +0530

    Deployed develop Storybook from Jenkins: https://deployment.limetray.com/ | Build Number: 46

---
+++ react-storybook-develop/assets/ltInput-944293e8.js
@@ -0,0 +1,78 @@
+import{j as a}from"./jsx-runtime-8406ef1e.js";import{r as n}from"./index-ad58220e.js";import{I as M}from"./Input-94f4c1a5.js";import{D as V}from"./Dropdown-66d5c8fe.js";import{d as A,l as P,o as W}from"./styled-components.browser.esm-675ea4f2.js";import{L as B}from"./index-f03014ac.js";import{t as F}from"./config-d270d9b6.js";const H=A(M)`
+  &&& {
+    border: 1px solid ${e=>e.theme.colors.border};
+    border-radius: ${e=>e.$borderRadius||e.theme.borderRadius};
+    overflow: ${e=>e.label?"visible":"hidden"};
+    transition: outline-color 0.2s ease, outline-offset 0.2s ease;
+
+    ${e=>e.$showOutlineOnFocus&&P`
+        &:focus-within {
+          outline-color: ${e.$outlineColor||e.theme.colors.primary};
+          outline-offset: ${e.$outlineOffset||"1.5px"};
+          outline-style: ${e.$outlineStyle||"solid"};
+          outline-width: ${e.$outlineWidth||"1.5px"};
+        }
+      `}
+
+    > input {
+      border: none;
+      border-radius: ${e=>e.$borderRadius||e.theme.borderRadius};
+      box-shadow: none;
+    }
+
+    > input:focus {
+      border-color: ${e=>e.theme.colors.focusBorder};
+      box-shadow: none;
+    }
+
+    > .button-text {
+      border: none;
+      border-radius: ${e=>e.$borderRadius||e.theme.borderRadius};
+      background-color: ${e=>e.theme.colors.background};
+      color: ${e=>e.theme.colors.primary};
+      font-weight: 100;
+    }
+
+    > .ui.dropdown {
+      border: none;
+      border-radius: ${e=>e.$borderRadius||e.theme.borderRadius};
+      background-color: ${e=>e.theme.colors.background};
+      font-weight: 300;
+    }
+
+    > .ui.dropdown > .menu {
+      border: none;
+      border-radius: 12px;
+      background-color: ${e=>e.theme.colors.background};
+      color: ${e=>e.theme.colors.primary};
+      font-weight: 100;
+      padding: 0.5em 0;
+    }
+
+    > .icon {
+      cursor: default;
+      position: absolute;
+      line-height: 1;
+      text-align: center;
+      top: 0;
+      right: 0;
+      margin: 0;
+      height: 100%;
+      width: 2.67142857em;
+      opacity: 0.5;
+      transition: opacity 0.3s ease;
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    > input:focus ~ .icon {
+      opacity: 1;
+    }
+
+    &[class*='left icon'] > .icon {
+      right: auto;
+      left: 1px;
+    }
+  }
+`,_=({placeholder:e,type:y,onChange:l,buttonText:u,onButtonClick:d,icon:t,iconPosition:h,borderRadius:v,showOutlineOnFocus:w=!1,outlineColor:$,outlineOffset:q,outlineStyle:x,outlineWidth:T,defaultValue:o="",className:R="",dropdownOptions:m,labelPlaceholder:I,onLabelClick:c,labelDefaultValue:i,...k})=>{const[p,f]=n.useState(o),[C,g]=n.useState(i),[E,b]=n.useState(!1);n.useEffect(()=>{f(o)},[o]),n.useEffect(()=>{g(i)},[i]);const O=(s,r)=>{f(s.target.value),l&&l(s,r)},S=()=>{d&&d(p)},L=(s,r)=>{g(r.value),c&&c(r.value)},j=()=>{b(!1)},N=()=>{b(!0)},D=n.isValidElement(t)?n.cloneElement(t,{className:[t.props.className,"icon"].filter(Boolean).join(" ")}):t;return a.jsx(W,{theme:F,children:a.jsx(H,{className:`lt-input ${R}`,$borderRadius:v,$showOutlineOnFocus:w,$outlineColor:$,$outlineOffset:q,$outlineStyle:x,$outlineWidth:T,placeholder:e,icon:D,type:y,iconPosition:h==="left"?"left":void 0,value:p,onChange:O,action:u?{content:u,onClick:S,className:"button-text"}:void 0,label:m&&a.jsx(V,{options:m,onChange:L,placeholder:I,onClose:j,onOpen:N,value:C,icon:a.jsx(B,{useReactIcon:!0,reactIconName:E?"MdExpandLess":"MdExpandMore",reactIconSet:"md",size:"small",style:{marginLeft:"0.5em",color:"#0000008A"},color:"#0000008A"})}),...k})})};_.__docgenInfo={description:"",methods:[],displayName:"LtInput",props:{placeholder:{required:!1,tsType:{name:"string"},description:""},buttonText:{required:!1,tsType:{name:"string"},description:""},onButtonClick:{required:!1,tsType:{name:"signature",type:"function",raw:"(value: string) => void",signature:{arguments:[{type:{name:"string"},name:"value"}],return:{name:"void"}}},description:""},icon:{required:!1,tsType:{name:"ReactReactElement",raw:"React.ReactElement<{ className?: string }>",elements:[{name:"signature",type:"object",raw:"{ className?: string }",signature:{properties:[{key:"className",value:{name:"string",required:!1}}]}}]},description:""},iconPosition:{required:!1,tsType:{name:"union",raw:"'left' | 'right'",elements:[{name:"literal",value:"'left'"},{name:"literal",value:"'right'"}]},description:""},defaultValue:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"''",computed:!1}},className:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"''",computed:!1}},style:{required:!1,tsType:{name:"ReactCSSProperties",raw:"React.CSSProperties"},description:""},type:{required:!1,tsType:{name:"string"},description:""},borderRadius:{required:!1,tsType:{name:"string"},description:""},showOutlineOnFocus:{required:!1,tsType:{name:"boolean"},description:"",defaultValue:{value:"false",computed:!1}},outlineColor:{required:!1,tsType:{name:"string"},description:""},outlineOffset:{required:!1,tsType:{name:"string"},description:""},outlineStyle:{required:!1,tsType:{name:"string"},description:""},outlineWidth:{required:!1,tsType:{name:"string"},description:""},onChange:{required:!1,tsType:{name:"signature",type:"function",raw:"(event: React.ChangeEvent<HTMLInputElement>, data: InputOnChangeData) => void",signature:{arguments:[{type:{name:"ReactChangeEvent",raw:"React.ChangeEvent<HTMLInputElement>",elements:[{name:"HTMLInputElement"}]},name:"event"},{type:{name:"InputOnChangeData"},name:"data"}],return:{name:"void"}}},description:""},dropdownOptions:{required:!1,tsType:{name:"Array",elements:[{name:"signature",type:"object",raw:"{ key: string; text: string; value: string }",signature:{properties:[{key:"key",value:{name:"string",required:!0}},{key:"text",value:{name:"string",required:!0}},{key:"value",value:{name:"string",required:!0}}]}}],raw:"{ key: string; text: string; value: string }[]"},description:""},labelPlaceholder:{required:!1,tsType:{name:"string"},description:""},onLabelClick:{required:!1,tsType:{name:"signature",type:"function",raw:"(value: string | number | boolean | (string | number | boolean)[] | undefined) => void",signature:{arguments:[{type:{name:"union",raw:"string | number | boolean | (string | number | boolean)[] | undefined",elements:[{name:"string"},{name:"number"},{name:"boolean"},{name:"Array",elements:[{name:"unknown"}],raw:"(string | number | boolean)[]"},{name:"undefined"}]},name:"value"}],return:{name:"void"}}},description:""},labelDefaultValue:{required:!1,tsType:{name:"union",raw:"string | number | boolean | (string | number | boolean)[] | undefined",elements:[{name:"string"},{name:"number"},{name:"boolean"},{name:"Array",elements:[{name:"unknown"}],raw:"(string | number | boolean)[]"},{name:"undefined"}]},description:""}}};export{_ as L};
